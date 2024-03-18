2faffd82539f2189e3c0f5baebb1cacfb6e75933
Marcin Barc <marb637@gmail.com>
2024-03-18 17:16:04 +0100

    Language component added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import i18n from "i18n";
+import { Languages } from "./components/languages/Languages";
+
+function App() {
+  return (
+    <>
+      <Languages />
+    </>
+  );
+}
+
+export default App;
