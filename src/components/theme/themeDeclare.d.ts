433c8b412758e423cb0b32d02ac23ecdb3463a1c
Marcin Barc <marb637@gmail.com>
2024-04-10 20:47:17 +0200

    test

--- src/components/theme/themeDeclare.d.ts
+++
@@ -1,6 +0,0 @@
-import "@mui/material";
-import { ThemeOptions as MuiThemeOptions } from "@mui/material";
-
-declare module "@mui/material" {
-  export interface ThemeOptions extends MuiThemeOptions {}
-}
