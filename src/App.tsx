ff6802aa1bbb4892f6699f2b4b73918e51d16a8b
Marcin Barc <marb637@gmail.com>
2024-04-16 21:52:02 +0200

    import added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { CssBaseline, ThemeProvider as MuiThemeProvider } from "@mui/material";
